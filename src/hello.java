dc606bce7484f4b9e75e2585871735905e21447e
Jelena Kutepova <zmejuka07@gmail.com>
2023-10-30 20:59:05 +0100

    make addition calkulator

--- src/hello.java
+++ src/hello.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class hello {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    System.out.println(a+b);
+
   }
 }
